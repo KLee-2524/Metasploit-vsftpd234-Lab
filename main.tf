2de2f0d62858155d6229d1a9890b798c711aa709
KLee-2524 <klee.337x@gmail.com>
2025-09-22 11:47:40 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,16 +25,7 @@ variable "attendee_usernames" {
   type = list(string)
 
   default = [
-    "mtrogers@cpp.edu",
-    "dieu@cpp.edu",
-    "jif@cpp.edu",
-    "ibrispin@cpp.edu",
-    "bleslie@cpp.edu",
-    "josertorres@cpp.edu",
-    "jrwoline@cpp.edu",
-    "similian@cpp.edu",
-    "zhichengliao@cpp.edu",
-    "chung1@cpp.edu"
+    "rastley@cpp.edu"
   ]
 }
 
@@ -54,5 +45,6 @@ module "vsftpd234-lab" {
     route_table_id  = aws_route_table.FAST-route-table.id
 
 }
+
 
 
